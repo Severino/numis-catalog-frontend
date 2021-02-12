93d8eec158ffdb456330e56b2503d42cd4dd038d
Severin Opel <severin.opel@outlook.com>
2021-02-12 10:06:02 +0100

    Added new i18n texts.

--- src/i18n/de.js
+++ src/i18n/de.js
@@ -1,70 +1,131 @@
 const general = {
-    administration: "Admin-Bereich",
-    create_type: "Neuen Typ anlegen",
-    manage_properties: "Eigenschaften verwalten",
-    type_catalogue: "Typenkatalog",
-    longitude: "longitude",
-    latitude: "latitude",
-    online: "online",
-    offline: "offline",
-    server_status:"server status",
-    delete_submit: "Entfernen"
+    administration:
+        "Admin-Bereich",
+    create_type:
+        "Neuen Typ anlegen",
+    manage_properties:
+        "Eigenschaften verwalten",
+    type_catalogue:
+        "Typenkatalog",
+    longitude:
+        "Längengrad",
+    latitude:
+        "Breitengrad",
+    online:
+        "online",
+    offline:
+        "offline",
+    server_status:
+        "server status",
+    delete_submit:
+        "Entfernen"
 }
 
-
 const property = {
-    additional_persons: "Sonstige Personen",
-    circular_text: "Umschrift | Umschriften",
-    coin_master: "Münzherr",
-    caliph: "Kalif",
-    donativ: "donativ",
-    field_text: "Feld",
-    honorific: "Ehrenname | Ehrennamen",
-    material: "Material | Materialien",
-    mint_year: "Prägejahr",
-    mint: "Prägeort | Prägeorte",
-    mint_as_on_coin: "Prägeort wie auf Münze",
-    nominal: "Nominal | Nominale",
-    overlord: "Oberherr | Oberherren",
-    person: "Person | Personen",
-    procedure: "Herstellung | Herstellungsarten",
-    procedures: {
-        cast: "gegossen",
-        pressed: "gepresst",
+    additional_persons:
+        "Sonstige Personen",
+    backside:
+        "Rückseite",
+    circular_text:
+        "Umschrift | Umschriften",
+    coin_master:
+        "Münzherr",
+    caliph:
+        "Kalif",
+    cursive_script:
+        "kursive Schriftart?",
+    donativ:
+        "Geschenkmünze?",
+    field_text:
+        "Feld",
+    frontside:
+        "Avers",
+    honorific:
+        "Ehrenname | Ehrennamen",
+    material:
+        "Material | Materialien",
+    mint_year:
+        "Prägejahr",
+    mint:
+        "Prägeort | Prägeorte",
+    mint_as_on_coin:
+        "Prägeort wie auf Münze",
+    nominal:
+        "Nominal | Nominale",
+    overlord:
+        "Oberherr | Oberherren",
+    person:
+        "Person | Personen",
+    procedure:
+        "Herstellungsart | Herstellungsarten",
+    procedures:
+    {
+        cast:
+            "gegossen",
+        pressed:
+            "geprägt",
     },
-    sides: {
-        front: "Vorderseite",
-        back: "Rückseite"
+    sides:
+    {
+        front:
+            "Vorderseite",
+        back:
+            "Rückseite"
     },
-    title: "Titel",
-    type_id: "Typennummer | Typennummern",
+    special_features:
+        "Besonderheiten",
+    title:
+        "Titel",
+    type_id:
+        "sikka:būya- ID | sikka:būya-IDs",
+    treadwill_id:
+        "Treadwell-ID | Treadwell-IDs",
+    varieties:
+        "Varianten"
+}
+
+const roles = {
+    cutter: "Stempelschneider"
+    heir: "designierter Thronfolger des Kalifen",
+    warden: "Münzwardein"
 }
 
 const attribute = {
-    name: "Name | Namen",
-    longitude: "Längengrad",
-    latitude: "Breitengrad"
+    name:
+        "Name | Namen",
+    longitude:
+        "Längengrad",
+    latitude:
+        "Breitengrad"
 }
 
 const error = {
-    could_not_update_element: "Das angegebene Element konnte nicht aktualisiert werden. Die Datenbank könnte nicht erreichbar sein.",
-    loading_element: "Das angefragte Element konnte nicht geladen werden. Gehe zur Liste zurück oder erstelle ein neues Element hier. Kontaktiere einen Admin, falls das Problem weiterhin besteht."
+    could_not_update_element:
+        "Das angegebene Element konnte nicht aktualisiert werden. Die Datenbank könnte nicht erreichbar sein.",
+    loading_element:
+        "Das angefragte Element konnte nicht geladen werden. Gehe zur Liste zurück oder erstelle ein neues Element hier. Kontaktiere einen Admin, falls das Problem weiterhin besteht."
     ,
-    loading_list: "List konnte nicht geladen werden. Anscheinend ist die Datenbank nicht verfügbar."
+    loading_list:
+        "List konnte nicht geladen werden. Anscheinend ist die Datenbank nicht verfügbar."
 }
 
 const warning = {
-    list_is_empty: "Bisher sind keine Werte in dieser Liste."
+    list_is_empty:
+        "Bisher sind keine Werte in dieser Liste."
 }
 
 const form = {
-    create: "erstellen",
-    submit: "senden",
-    cancel: "abbrechen"
+    create:
+        "erstellen",
+    submit:
+        "senden",
+    cancel:
+        "abbrechen"
 }
 
 const navigation = {
-    back: "zurück"
+    back:
+        "zurück"
 }
 
 module.exports = {
