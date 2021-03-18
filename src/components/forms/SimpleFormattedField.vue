2247299cfa36a0f936bc078068965f13724c297c
Severin Opel <severin.opel@outlook.com>
2021-03-18 10:28:27 +0100

    Improved Simple Formatted Field.

--- src/components/forms/SimpleFormattedField.vue
+++ src/components/forms/SimpleFormattedField.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="simple-formatted-field">
     <Row class="toolbar" style="margin-bottom: 10px">
-      <button @click="align('left')">Left</button>
-      <button @click="align('center')">Center</button>
-      <button @click="align('right')">Right</button>
-      <span style="margin-right: 50px"></span>
-      <button @click="toggleBold">Bold</button>
-      <button @click="toggleCursive">Cursive</button>
+      <button @click="align('left')"><FormatAlignLeft /></button>
+      <button @click="align('center')"><FormatAlignCenter /></button>
+      <button @click="align('right')"><FormatAlignRight /></button>
+      <div class="spacer"></div>
+      <button @click="toggleBold"><FormatBold /></button>
+      <button @click="toggleCursive"><FormatItalic /></button>
     </Row>
 
     <div ref="field" class="formatted-text-area" contenteditable></div>
@@ -15,22 +15,37 @@
 
 <script>
 import Row from "../layout/Row.vue";
+
+import FormatAlignLeft from "vue-material-design-icons/FormatAlignLeft";
+import FormatAlignCenter from "vue-material-design-icons/FormatAlignCenter";
+import FormatAlignRight from "vue-material-design-icons/FormatAlignRight";
+
+import FormatBold from "vue-material-design-icons/FormatBold";
+import FormatItalic from "vue-material-design-icons/FormatItalic";
+
 export default {
-  components: { Row },
+  components: {
+    Row,
+    FormatAlignLeft,
+    FormatAlignCenter,
+    FormatAlignRight,
+    FormatBold,
+    FormatItalic,
+  },
   name: "SimpleFormattedField",
-  data: function () {
+  data: function() {
     return {
       range: null,
     };
   },
   methods: {
-    setContent: function (str) {
+    setContent: function(str) {
       this.$refs.field.innerHTML = str;
     },
-    getContent: function () {
+    getContent: function() {
       return this.$refs.field.innerHTML;
     },
-    getSelected: function () {
+    getSelected: function() {
       let node = document.getSelection().anchorNode;
 
       /**
@@ -57,7 +72,7 @@ export default {
       return node;
     },
 
-    align: function (value) {
+    align: function(value) {
       const node = this.getSelected();
       if (node) {
         Object.assign(node.style, {
@@ -66,15 +81,15 @@ export default {
         this.$emit("input", this.$refs.field.innerHTML);
       }
     },
-    toggleBold: function () {
+    toggleBold: function() {
       document.execCommand("bold", false, null);
       this.$emit("input", this.$refs.field.innerHTML);
     },
-    toggleCursive: function () {
+    toggleCursive: function() {
       document.execCommand("italic", false, null);
       this.$emit("input", this.$refs.field.innerHTML);
     },
-    input: function (event) {
+    input: function(event) {
       const target = event.target;
       this.$emit("input", target.innerHTML);
     },
@@ -82,7 +97,7 @@ export default {
     /**
      * Thankfully taken from: https://gist.github.com/dantaex/543e721be845c18d2f92652c0ebe06aa
      */
-    saveSelection: function () {
+    saveSelection: function() {
       if (window.getSelection) {
         var sel = window.getSelection();
         if (sel.getRangeAt && sel.rangeCount) {
@@ -93,7 +108,7 @@ export default {
       }
       return null;
     },
-    restoreSelection: function (range) {
+    restoreSelection: function(range) {
       if (range) {
         if (window.getSelection) {
           var sel = window.getSelection();
@@ -111,10 +126,26 @@ export default {
 <style lang="scss" scoped>
 @import "@/scss/_import.scss";
 
+.toolbar {
+  // background-color: rgb(100, 100, 100);
+  margin-bottom: 0  !important;
+  > *:not(.spacer) {
+    width: unset;
+    margin: 0 !important;
+    padding: 0;
+    padding-top: 3px;
+    background-color: rgb(100, 100, 100);
+    .material-design-icon__svg {
+      height: 18px;
+      color: $white;
+    }
+  }
+}
+
 .formatted-text-area {
   @include input();
   font-size: 1rem;
-  min-height: 240px;
+  min-height: 1em;
   // background-color: red;
 
   span {
